5a02b86b60820eb5ed91890fee8de53919102af6
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-08-23 02:27:49 +0530

    Create CEASER_CIPPHER.C

---
+++ practice_work/CEASER_CIPPHER.C
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+#include<ctype.h>
+
+int main() {
+
+    char text[500], ch;
+
+    int key;
+
+    // Taking user input.
+    printf("Enter a message to encrypt: ");
+
+    scanf("%s", text);
+
+    printf("Enter the key: ");
+
+    scanf("%d", & key);
+
+    // Visiting character by character.
+
+    for (int i = 0; text[i] != '\0'; ++i) {
+
+        ch = text[i];
+        // Check for valid characters.
+        if (isalnum(ch)) {
+
+            //Lowercase characters.
+            if (islower(ch)) {
+                ch = (ch - 'a' + key) % 26 + 'a';
+            }
+            // Uppercase characters.
+            if (isupper(ch)) {
+                ch = (ch - 'A' + key) % 26 + 'A';
+            }
+
+            // Numbers.
+            if (isdigit(ch)) {
+                ch = (ch - '0' + key) % 10 + '0';
+            }
+        }
+        // Invalid character.
+        else {
+            printf("Invalid Message");
+        }
+
+        // Adding encoded answer.
+        text[i] = ch;
+
+    }
+
+    printf("Encrypted message: %s", text);
+
+    return 0;
+}
