a04f591f2f2693bfdda6c383eebe2898141c33cc
Nikhil's <130584896+Nikhils-G@users.noreply.github.com>
2024-08-23 22:48:47 +0530

    Create ishould.c

---
+++ strings/ishould.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[]="abac";
+    char str2[]="xx";
+    if(strstr(str1,str2) != NULL)
+    {
+        printf("NIKHIL");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
